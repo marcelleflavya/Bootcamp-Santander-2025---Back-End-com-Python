e31442578ee5ab5bb93840561f9d04c6dcad7f2c
marcelleflavya <marcelleflavya@gmail.com>
2025-07-17 14:37:16 -0300

    banco com funções e atualização de nome

--- organizando_evento.py
+++ organizando_evento.py
@@ -1,10 +1,13 @@
+#Uma empresa quer criar um organizador de eventos que 
+# divida os participantes em grupos de acordo com o tema escolhido
 # Dicionário para agrupar participantes por tema
 eventos = {}
 
 
 n = int(input().strip())
 
-
+#Crie um loop para armazenar participantes e seus temas:
+#
 for _ in range(n):
     linha = input().strip()
     nome, tema = [x.strip() for x in linha.split(',')]
